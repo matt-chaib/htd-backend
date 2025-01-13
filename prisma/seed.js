208be292fb0f0a2fb3382a5901d09cb4639516c0
mattchaib <mattchaib@gmail.com>
2025-01-13 20:40:34 +0000

    fix improts

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,8 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import prisma from "../src/prismaClient.js";
 
 async function main() {
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
   const questionsData = JSON.parse(
     fs.readFileSync(path.join(__dirname, '../questions.json'), 'utf8')
